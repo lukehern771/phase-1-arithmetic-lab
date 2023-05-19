90bf672fac2cd9cbe225a9c2ab9c730700b6b2e9
Luke <hluke9242@gmail.con>
2023-05-19 12:29:44 -0600

    complete lab phase-1

--- index.js
+++ index.js
@@ -1,2 +1,11 @@
+const multiply = 2 * 31;
+let num1 = 2
+let num2 = 31
 
-// Write your code here
+const random = Math.ceil(Math.random()*10) + 1;
+
+const mod = 20 % 8;
+let num3 = 20
+let num4 = 8
+
+const max = Math.max(5, 10, 15, 20);
